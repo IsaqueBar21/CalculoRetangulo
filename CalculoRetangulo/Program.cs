01dcb68e2b4d9e74e58a13cf3261c41e4e3bbd4b
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-10-12 07:23:43 -0300

    primeiro commit

---
+++ CalculoRetangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+double b, a;
+
+b = double.Parse(Console.ReadLine());
+a = double.Parse(Console.ReadLine());
+
+double area = b * a;
+double perimetro = (2 * b) + (2 * a);
+double diagonal = Math.Sqrt(Math.Pow(b,2) + Math.Pow(a,2));
+
+Console.WriteLine("Area = " + area.ToString("f4", CultureInfo.InvariantCulture));
+Console.WriteLine("Perímetro = " + perimetro.ToString("f4", CultureInfo.InvariantCulture));
+Console.WriteLine("Diagonal = " + diagonal.ToString("f4", CultureInfo.InvariantCulture));
+
+
+
+
